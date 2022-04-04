1adf2f65fa198f794f53cb45764e4b708d458c9b
JailsonSantos <jailson.ads007@gmail.com>
2022-04-04 17:12:51 -0300

    Atulização da home

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -4,10 +4,12 @@ import Marker from '../marker';
 
 const Map = () => {
 
+  // Minha Chave API - GoogleMapReact
+  // key: "AIzaSyAJav92MVud31_U4j3MJsKA7788v8pyDLc"
   return (
     <div className="container-map">
       <GoogleMapReact
-        bootstrapURLKeys={{ key: '' }}
+        bootstrapURLKeys={{ key: 'AIzaSyCWBxlNpEtAk1yi9lgZ5WeW89b5pdva0Ek' }}
         center={{
           lat: -2.526949,
           lng: -44.252671,
